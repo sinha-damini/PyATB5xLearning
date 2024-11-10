ab3a660da1b66c2c31c409ba4621ee3d1725b66a
sinha-damini <damini802301sinha@gmail.com>
2024-11-10 18:40:24 +0530

    Initial commit

---
+++ Nov/ex_07112024_keywords_identifiers_variables/Lab007_Variable_Identifier_Rules.py
@@ -0,0 +1,3 @@
+age = 65 # integer -> int
+pi = 3.14 # float - decimal
+name = "Damini" # string - bunch of characters -> str
---
+++ Nov/ex_07112024_keywords_identifiers_variables/Lab010.py
@@ -0,0 +1,5 @@
+long_var_name_is_created_here  = "Long Variable Name"
+print(long_var_name_is_created_here)
+
+# space is not allowed  = "Space not allowed"
+# print(space is not allowed)
