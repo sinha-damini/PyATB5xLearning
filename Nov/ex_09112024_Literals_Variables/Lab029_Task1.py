c4f804c1b04df9dcec15233d11e1f45ff4396330
sinha-damini <damini802301sinha@gmail.com>
2024-11-12 00:47:37 +0530

    10th Nov Part2

---
+++ Nov/ex_09112024_Literals_Variables/Lab029_Task1.py
@@ -0,0 +1,29 @@
+
+#Task for Today
+#Take 3 i/p from the user
+#perform the sum,sub,mul and div
+
+#num1 = 60
+#num2 = 5
+#num3 = 3
+
+num1 = int(input("Enter the number 1 "))
+num2 = int(input("Enter the number 2 "))
+num3 = int(input("Enter the number 3 "))
+
+print("sum of given number =", num1+num2+num3)
+print("sub of given number =", num1-num2-num3)
+print("mul of given number =", num1*num2*num3)
+print("Div of given number =", num1/num2/num3)
+
+'''
+Enter the number 1 60
+Enter the number 2 5
+Enter the number 3 3
+sum of given number = 68
+sub of given number = 52
+mul of given number = 900
+Div of given number = 4.0
+
+
+'''
---
+++ Nov/ex_09112024_Literals_Variables/Lab030_Task2.py
@@ -0,0 +1,26 @@
+#Task for Today
+#Take 2 i/p from the user
+#print the uotient and remainder.
+#num1 - 15
+#num2 - 2
+#Q - 7
+#R - 1
+
+num1 = int(input("Enter the number 1 "))
+num2 = int(input("Enter the number 2 "))
+
+print("Quotient of given number =", num1//num2)
+print("Remainder of given number =", num1%num2)
+
+'''
+// -> floor division
+% -> modulus operator
+'''
+
+"""
+Enter the number 1 15
+Enter the number 2 2
+Quotient of given number = 7
+Remainder of given number = 1
+
+"""
